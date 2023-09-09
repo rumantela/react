a0c087b138ed64d62e336d872c489de068050c07
Adolfo Sanchez <adolfosanchezlopez@gmail.com>
2023-09-09 14:48:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from "https://esm.sh/react-dom?pin=v132";
+
+const appDomElement = document.getElementById('app');
+
+const root = ReactDom.createRoot(appDomElement);
+root.render("Hola React!")
